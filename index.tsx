2a94ae0282f09c183885ee45b64ba0b58a93804f
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2019-08-16 15:29:15 +0300

    initial commit

---
+++ index.tsx
@@ -0,0 +1,10 @@
+import { h, render } from "preact";
+
+const App = () => (
+  <div>
+    <h1>Hello World from Preact! 📦 🚀</h1>
+  </div>
+);
+
+const node = document.getElementById("root");
+render(<App />, node!, node!.lastChild as Element);
